7eef4182f277d652382fec04624c2bf3b52efdfc
EJCPMJP <142831189+EJCPMJP@users.noreply.github.com>
2025-09-08 13:06:37 -0300

    Refactor and fix build issues

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,3 @@
-@'
-export { handlers as GET, handlers as POST } from "@/lib/auth";
-'@ | Set-Content .\src\app\api\auth\[...nextauth]\route.ts -Encoding utf8
+import { handlers } from "@/lib/auth";
+
+export const { GET, POST } = handlers;
